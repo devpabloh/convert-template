f68fd9478ec1c82cd2e3febb29fa78b1375ef829
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-11-06 15:03:55 -0300

    retrieving the currency selected by the user

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
+const form = document.querySelector("form")
 const  amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Manipulando o input amount(quantia) para receber somento números
 amount.addEventListener("input", (event)=>{
-    event.preventDefault()
+    
     hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
     console.log(amount.value)
 })
+
+// capturando o evento de submit(enviar) do formulário
+form.onsubmit = (event)=>{
+    event.preventDefault() // não ficar acontecendo o reconect da página
+
+    console.log(currency.value)
+}
